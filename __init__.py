3b5b78b43c9575f1fa14a491b9f563f07109ec31
Ljbracho2021 <83231659+Ljbracho2021@users.noreply.github.com>
2022-05-19 17:25:16 -0400

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from . import ubicacion, territorio, persona, activo, accion, sector, direccion, organizacion, rol, caso, clap, ubch, calle, casa, familia
+from . import estado
+from . import municipio
+from . import comuna
+from . import comunidad
+from . import unidad
+from . import categoria
+from . import centro
+from . import tipoa
+from . import acciong
+from . import tipov
+from . import parentesco
+from . import parroquia
+from . import rwaccion
+from . import rwtipo
+
+
+
